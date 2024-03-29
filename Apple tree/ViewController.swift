26491d35fe189134e31ad23d294d70f1b957134b
SSFSKIM <kimmi@ssfs.org>
2024-03-29 13:25:34 -0400

    Initial Commit

---
+++ Apple tree/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Apple tree
+//
+//  Created by Steve on 3/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var socreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet var treeImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
